28c4bf2d960f5c0f95ea7c413ac3f2b968c04896
Harsh Pandya <hpand3@gmail.com>
2019-08-28 11:49:15 +1000

    HP - Add data generation

---
+++ data/data_gen.py
@@ -0,0 +1,28 @@
+import os
+import random
+
+import psycopg2
+
+moods = ['happy', 'sad']
+rows_to_generate = int(os.environ['GEN_ROWS'])
+
+conn = psycopg2.connect("host=postgresdb dbname=postgres user=postgres password=mysecretpassword")
+print('Connected to DB')
+
+cur = conn.cursor()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS mood_table (
+    age INTEGER NOT NULL,
+    mood VARCHAR(255) NOT NULL
+)
+""")
+conn.commit()
+
+for _ in range(rows_to_generate):
+    age = random.randint(13,99)
+    mood = random.choice(moods)
+    cur.execute(f"INSERT INTO mood_table VALUES ({age}, '{mood}')")
+conn.commit()
+
+print('Insert complete')
+cur.close()
